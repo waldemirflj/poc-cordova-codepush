b57805686832b4e6bd9111e87b80b0494f4098d9
waldemirflj <waldemirflj@gmail.com>
2019-05-16 01:40:59 -0300

    integração com App Center Push

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,11 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        // App Center: Push
+        this.appCenterPush();
+        this.enablePush();
+        this.checkPush();
     },
 
     // Update DOM on a Received Event
@@ -40,6 +45,65 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    appCenterPush: function() {
+        const onNotificationReceived = (pushNotification) => {
+            let message = pushNotification.message;
+            let title = pushNotification.title;
+    
+            if (message === null || message === undefined) {
+                // Android messages received in the background don't include a message. On Android, that fact can be used to
+                // check if the message was received in the background or foreground. For iOS the message is always present.
+                title = 'Android background';
+                message = '<empty>';
+            }
+    
+            // Custom name/value pairs set in the App Center web portal are in customProperties
+            if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+                message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+            }
+            
+            console.log(title, message);
+        };
+
+        AppCenter.Push.addEventListener('notificationReceived', onNotificationReceived);
+    },
+
+    enablePush: function() {
+        const enableSuccess = () => {
+            console.log("push notifications enabled");
+        }
+        
+        const disableSuccess = () => {
+            console.log("push notifications disabled");
+        }
+        
+        const error = (error) => {
+            console.error(error);
+        }
+
+        const checkbox = document.querySelector('#enablePush');
+        
+        checkbox.addEventListener('change', () => {
+            if(checkbox.checked) {
+                AppCenter.Push.setEnabled(true, enableSuccess, error);
+            } else {
+                AppCenter.Push.setEnabled(false, disableSuccess, error);
+            }
+        });
+    },
+
+    checkPush: function() {
+        const success = (result) => {
+            console.log("push notifications " + (result) ? "enabled" : "disabled");
+        }
+        
+        const error = (error) => {
+            console.error(error);
+        }
+        
+        AppCenter.Push.isEnabled(success, error);
     }
 };
 
